9a2cbec3fa2fa1a4ee2561c2f556cc1bd685c1dd
yehia67 <yehiatarek67@gmail.com>
2019-09-24 18:38:53 +0200

    done fetching root

--- prototypes/Send ID/FetchPublicTransaction.js
+++ prototypes/Send ID/FetchPublicTransaction.js
@@ -8,13 +8,16 @@ const Converter = require('@iota/converter')
 const iota = iotaLibrary.composeAPI({
   provider: 'https://nodes.devnet.iota.org:443'
 })
-
-const FetchPublicTransaction =  (_address) =>{
-  
+let msgInfo
+const FetchPublicTransaction = async (_address) =>{
+   return new Promise(function(resolve, reject) {
       iota
-      .findTransactionObjects({ addresses: [_address] })
-      .then(response => {
-        console.log('Encoded message:')
+      .findTransactionObjects({ addresses: [_address] },function(error, response) {
+        if (error) {
+          reject(error);
+      } else {
+        
+         console.log('Encoded message:')
         console.log(response[0].signatureMessageFragment)
     
         // Modify trytes into a consumable length
@@ -22,14 +25,13 @@ const FetchPublicTransaction =  (_address) =>{
         //Convert trytes to plan text
         //Convert trytes to plan text
         const data = Converter.trytesToAscii(trytes)
+        msgInfo = data
         console.log('Decoded message:')
-        console.log(data)
-        return data
-      })
-      .catch(err => {
-        console.error(err)
-      })
-
+        console.log(data) 
+        resolve(data)
+      }
+    }) 
+   })
     }
 module.exports ={
   execute:FetchPublicTransaction
--- prototypes/Send ID/fetchID.js
+++ prototypes/Send ID/fetchID.js
@@ -1,11 +1,8 @@
 const FetchPublicTransaction = require('./FetchPublicTransaction.js')
 const FetchMamRestricted = require('./FetchMamRestricted.js')
 
-const address = 'YEHIAJCB9COPCTMOIFUEJVGUNDLSSJQCORYMEAZHRAUJXH9HGNI99BMLQDQOZNCLNVOLFMSFUIVPMYXDWEZFSZQROC'
+const address = 'YEHIA9CB9COPCTMOIFUEJVGUNDLSSJQCORYMEAZHRAUJXH9HGNI99BMLQDQOZNCLNVOLFMSFUIVPMYXDWEZFSZQROC'
 
-const main = async() => {
-    const root = await FetchPublicTransaction.execute(address)
-    console.log("your root is ", root)
-    //FetchMamRestricted.execute(root)
-}
-main()
+const root =  FetchPublicTransaction.execute(address).then(function(result){  
+        FetchMamRestricted.execute(result)
+    })
