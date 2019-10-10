434f42717d6a659e84b8869ab4ea4f30a2d82b49
yehia67 <yehiatarek67@gmail.com>
2019-10-10 17:11:12 +0200

    write testing section and modify deployement section

--- DevOps/CiCd.sh
+++ DevOps/CiCd.sh
@@ -1,8 +1,10 @@
 #Test
-
+echo "parsing tests.."
+cd ../test/mainTest.js
+cd ../simulate/mainSimulator.js
 
 #Commit
-echo enter your Commit
+echo "enter your Commit"
 read Commit
 git add .
 git commit -m "$Commit"
@@ -10,3 +12,10 @@ git push -u origin master
 
 #Deploy 
 ipfs add -r ../src/
+echo "Press y to deploy"
+read ip
+if [ "$ip" == "Y" ] || [ "$ip" == "y" ]; then
+   echo "input word Y"
+else
+   echo "The file 'somefile' does not exist."
+fi
