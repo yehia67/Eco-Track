bf8cb0461962bc5a3fd9fe6b91a1a052cd8353eb
yehia67 <yehiatarek67@gmail.com>
2019-10-15 10:47:06 +0200

    remove all unsuded function and start refactoring root and controllers file

--- prototypes/track-api/api/controllers/todoListController.js
+++ prototypes/track-api/api/controllers/todoListController.js
@@ -1,53 +1,23 @@
 'use strict';
 
 
-var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+//@name
+//@date
+//@noOfItems
+const generateMainID =(_name,_noOfItems) =>{
+        let mainID = ''
+        const name = _name.split(' ')
+        for (let index = 0; index < name.length; index++) {
+            mainID += name[index].charAt(0)
+        }
+      return mainID.replace(/ /g,'')+ Math.floor((Math.random() * 9000) + 1000) +String(_noOfItems)
+}
+
+
 
 exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
+    const mainID = generateMainID(req.name,req.noOfItems)
+    
+}
 
 
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-
-exports.delete_a_task = function(req, res) {
-
-
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Task successfully deleted' });
-  });
-};
