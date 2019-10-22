8a4cd5185ce9078a22ce14fba7b4df9058ae5a29
yehia67 <yehiatarek67@gmail.com>
2019-10-22 17:16:01 +0200

    make create prodct route more clean

--- prototypes/track-api/api/controllers/trackerController.js
+++ prototypes/track-api/api/controllers/trackerController.js
@@ -1,20 +1,10 @@
 'use strict';
 const initComm = require('./iota_comm_modules/initComm')
-const generateMainID =(_name,_noOfItems) =>{
-        let mainID = ''
-        const name = _name.split(' ')
-        for (let index = 0; index < name.length; index++) {
-            mainID += name[index].charAt(0)
-        }
-      return mainID.replace(/ /g,'')+ Math.floor((Math.random() * 9000) + 1000) +String(_noOfItems)
-}
 
 
-exports.create_products = function(req, res) {
-     console.log('___________________________________________________',req.body.products)  
+
+exports.create_products = (req, res) => {
      initComm.execute(req.body.products).then(function(tracker) {
-       console.log("***************************",tracker)
-       console.log('type-------------',typeof(req.body))
       res.json(tracker)  
     }) 
 }
