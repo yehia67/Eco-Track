ca1fb6645d9042a7a5bc2ddfd05c943186767f08
yehia67 <yehiatarek67@gmail.com>
2019-11-18 13:43:48 +0200

    detect nfs tags

--- prototypes/NFC_READER/app/src/main/java/com/example/nfc_reclying_products/MainActivity.java
+++ prototypes/NFC_READER/app/src/main/java/com/example/nfc_reclying_products/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.nfc_reclying_products;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    NfcAdapter nfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if(nfcAdapter!=null && nfcAdapter.isEnabled()){
             Toast.makeText(this, "NFC available!", Toast.LENGTH_LONG).show();
@@ -20,5 +23,35 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "NFC not available :( ", Toast.LENGTH_LONG).show();
         }
     }
+    @Override
+    protected void onNewIntent(Intent intent) {
 
+        Toast.makeText(this, "NFc intent received!", Toast.LENGTH_LONG).show();
+
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[] {};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter,
+                null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        nfcAdapter.disableForegroundDispatch(this);
+
+        super.onPause();
+    }
 }
