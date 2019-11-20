f569c966aef25f61884c0f00ea58bf8b4144e91f
yehia67 <yehiatarek67@gmail.com>
2019-11-20 16:44:18 +0200

    remove getAddresses function used to send data to recycler module

--- prototypes/track-api/api/controllers/trackerController.js
+++ prototypes/track-api/api/controllers/trackerController.js
@@ -8,7 +8,6 @@ exports.get_products = async (req,res) =>{
  }
 exports.create_products = (req, res) => {
       initComm.execute(req.body.products).then(function(tracker) {
-      console.log('yaraaaaaab yaaraaaaaab',initComm.getAddresses())          
       res.json(tracker)  
     }) 
 }
